3545ab98339d6121c4a82d2deb440168b7fb8a3f
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2022-08-22 16:35:02 -0400

    added cat and filtering scripts

---
+++ 01_scripts/00_cat_fastq_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Merge fastq.gz files for each sample
+# Requires that all fasta files for a given sample are in a folder in 04_raw_data named with sample ID, e.g. 04_raw_data/13013A
+# parallel -a 02_infos/samples_list.txt -j 10 srun -p small -c 1 --mem=20G -J {}_00_cat_fasta_files -o log/00_cat_fasta_files_{}_%j.log 01_scripts/00_cat_fastq_files.sh {} &
+
+# VARIABLES
+SAMPLE=$1
+CAT_DIR="04_raw_data_cat"
+RAW_DATA_DIR="04_raw_data/$SAMPLE"
+
+
+# 1. Merge fasta files for each sample
+cat $RAW_DATA_DIR/*.fastq.gz > $CAT_DIR/"SAMPLE".fastq.gz
---
+++ 01_scripts/01_NanoFilt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+# Quality filter raw ONT reads from merged files
+# parallel -a 02_infos/ind_ONT.txt -j 4 srun -p small -c 1 --time=1-00:00 -J {}_01_NanoFilt --mem=20G -o log/01_NanoFilt_{}_%j.log 01_scripts/01_NanoFilt.sh {} &
+
+# VARIABLES
+SAMPLE=$1
+CAT_DATA="04_raw_data_cat" 
+FASTQ="$RAW_DATA/"$SAMPLE".fastq.gz" # merged fastq.gz
+
+FILT_DIR="05_filtered"
+
+MIN_LEN=1000 # exclude reads shorter than this
+MIN_QUAL=10 # exclude reads blow this quality threshold
+
+
+# Create output directory
+if [[ ! -d "$OUT_DIR" ]]
+then
+  echo "$OUT_DIR does not exist"
+  mkdir $OUT_DIR
+else 
+  echo "$OUT_DIR exists"
+fi
+
+
+# 1. Run NanoFilt
+gunzip -c $FASTQ | NanoFilt -q $MIN_QUAL -l $MIN_LEN --readtype 1D | gzip > $FILT_DIR/"$SAMPLE"_min"$MIN_LEN"bp_qual"$MIN_QUAL".fastq.gz
+#gunzip -c $FASTQ | NanoFilt -q $MIN_QUAL -l $MIN_LEN --readtype 1D | gzip > $FILT_DIR/"$SAMPLE".fastq.gz
+
